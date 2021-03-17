a41a30822a5bb37096fa399f0b0dc5a6c574dc20
AlexandreFsilva <alex-afs@hotmail.com>
2021-03-16 21:13:54 -0300

    Criando comida da cobrinha

--- Bootstrap/js/main.js
+++ Bootstrap/js/main.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 *box
 }
 let direction = "right" ;
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 function criarBg() { // cria o plano de fundo
 context.fillStyle = "lightgreen"; // Fill Style é o estilo do contexto, no caso verde
 context.fillRect(0, 0, 16 * box, 16 * box); // fill rect definiu o conjunto de quadrados do canvas no caso 16x 32pixels
@@ -20,13 +24,17 @@ function criarCobrinha(){
     }
 } 
 
+function drawfood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
 document.addEventListener('keydown', update);
 
 function update(event){
     if(event.keyCode == 37 && direction !="right") direction ="left";
     if(event.keyCode == 38 && direction !="down") direction ="up";
     if(event.keyCode == 39 && direction !="left") direction="right";
-    if(event.keyCode == 40 && direction !="") direction = "down";
+    if(event.keyCode == 40 && direction !="up") direction = "down";
 }
 
 function iniciarJogo(){
@@ -36,6 +44,7 @@ if( snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
 if( snake[0].y < 0  && direction == "up") snake[0].y = 16 * box;
 criarBg();
 criarCobrinha();
+drawfood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
